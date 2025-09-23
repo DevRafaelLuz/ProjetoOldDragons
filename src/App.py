7a70ad5df122554c61593b85602e9ab0e78ad096
DevRafaelLuz <rafael.luz2005@gmail.com>
2025-09-23 20:31:49 -0300

    Ajuste na rota da página de criar personagem

--- src/App.py
+++ src/App.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/criacao-personagem', methods=['GET', 'POST'])
+@app.route('/personagem', methods=['GET', 'POST'])
 def criacao_personagem():
     estilo = ''
     nome = ''
